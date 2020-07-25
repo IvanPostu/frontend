24619ff772dcdadbe71c39efa2797ea4e9d4a53d
IvanPostu <ipostu20000127@gmail.com>
2020-07-25 22:02:34 +0300

    Added library for mocking.

--- src/cards1/hello.test.js
+++ src/cards1/hello.test.js
@@ -1,7 +1,28 @@
 import { sayHello } from './hello.js'
+import { mock } from 'sinon';
 import test from 'ava';
 
 test('sayHello function test', t => {
   const helloRick = sayHello('Rick')
   t.is(helloRick, 'Hello Rick')
 })
+
+const jonny = {
+  work: function(){
+    return 'I work.'
+  }
+}
+
+test('test case with mock', t => {
+  const m = mock(jonny)
+  m.expects('work').once().returns('Mock value')
+
+  const val = jonny.work()
+  t.is(val, 'Mock value')
+
+  jonny.work.restore()
+
+  const val2 = jonny.work()
+  t.is(val2, 'I work.')
+})
+
